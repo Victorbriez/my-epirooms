edd7a52e6b6e4d77fbb82051f101c53bda359d34
Victorbriez <victor.briez@epitech.eu>
2025-03-31 12:11:41 +0200

    feat: Add Calendar icon and enhance next activity display in ActivityInfo component

--- components/activity-panel/activity-info.tsx
+++ components/activity-panel/activity-info.tsx
@@ -1,4 +1,4 @@
-import { Clock } from "lucide-react";
+import { Clock, Calendar } from "lucide-react";
 import type { Activity } from "@/models/Activity";
 import { Separator } from "@/components/ui/separator";
 
@@ -75,10 +75,36 @@ export function ActivityInfo({ activities, currentTime }: ActivityInfoProps) {
             </div>
           </div>
         </div>
+      ) : nextActivity && nextActivity.length > 0 ? (
+        <div className="bg-green-200 p-3 rounded-lg shadow-sm">
+          <h3 className="font-extrabold text-green-800">
+            {nextActivity[0].title}
+          </h3>
+          <Separator className="my-2 bg-green-800" />
+          <div className="flex items-center justify-between gap-2 mt-2">
+            <div className="flex items-center gap-1 text-green-800">
+              <Clock className="w-4 h-4 flex-shrink-0" />
+              <p className="text-sm font-semibold">
+                {nextActivity[0].formatTime()}
+              </p>
+            </div>
+            <div className="flex items-center gap-1 text-green-800">
+              <Clock className="w-4 h-4 flex-shrink-0" />
+              <p className="text-sm">
+                Dans{" "}
+                <span className="font-semibold">
+                  {formatTimeDifference(
+                    nextActivity[0].getTimeUntilStart(currentTime)
+                  )}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       ) : (
         <div className="bg-green-200 p-3 rounded-lg shadow-sm">
           <div className="flex items-center gap-2 text-green-800">
-            <Clock className="w-4 h-4 flex-shrink-0" />
+            <Calendar className="w-4 h-4 flex-shrink-0" />
             <p className="font-extrabold">Libre pour le reste de la journée</p>
           </div>
         </div>
